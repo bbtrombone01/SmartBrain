1ee8c2625df19a7ffa2acbb63529570189a3a9e7
bbtrombone <bbtrombone@sbcglobal.net>
2022-08-16 14:09:41 -0700

    added Ranking folder and Componet

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkform from './components/imageLinkForm/ImageLinkForm';
+import Rank from './components/rank/Rank';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
 
     <Navigation />
     <Logo />
+    <Rank />
     <ImageLinkform />
 
       {/*
